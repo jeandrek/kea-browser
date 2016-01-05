1f8979cb96309451d793ebb5b7b31eab68968576
hiccup01 <hiccup01@users.noreply.github.com>
2016-01-06 11:03:17 +1300

    Update macosxinstaller.sh

--- macosxinstaller.sh
+++ macosxinstaller.sh
@@ -30,9 +30,10 @@ sleep 3
 echo "Attempting to install brew package mananger, if brew is already installed this will have no effect"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "Installing kea dependencies"
-brew install webkitgtk unp wget gnu-tar gzip 
+brew install webkitgtk unp wget gnu-tar 
 read -p "What directory would like to install kea in? The default and recommended is ~" DIRECTORY #Ask for user input
-if [ -n "$DIRECTORY" ]; then
+if [ -n "$DIRECTORY" ]
+then
 	echo "Installing in directory $DIRECTORY"
 else
 	echo "Defaulting. Installing in ~"
